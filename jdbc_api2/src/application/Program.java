e34e789fcd89b46928e7265435f3288a4e479c98
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-23 21:00:58 -0300

    clone do projeto jdbc_api,Demo - recuperação de dados

---
+++ jdbc_api2/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+	
+
+	public static void main(String[] args) {
+		//testa a conexão
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+}
